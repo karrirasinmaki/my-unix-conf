c7aa79d741cc7feee4cb70851eb13dca8881a83b
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2016-07-22 11:42:01 +0300

    Gitignore and gitconfig

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ echo $BASEDIR
 ln -sfv $BASEDIR/.zshrc ~/.zshrc
 ln -sfv $BASEDIR/.myrc ~/.myrc
 ln -sfv $BASEDIR/.vimrc ~/.vimrc
+ln -sfv $BASEDIR/.gitconfig ~/.gitconfig
+ln -sfv $BASEDIR/.gitignore ~/.gitignore
