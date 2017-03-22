65850a59ff1a79f0466eb42e646c122f80e74f2a
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2017-03-22 11:16:28 +0200

    Added Vundle install into install script

--- install.sh
+++ install.sh
@@ -11,5 +11,8 @@ ln -sfv $BASEDIR/.vimrc ~/.vimrc
 ln -sfv $BASEDIR/.gitconfig ~/.gitconfig
 ln -sfv $BASEDIR/.gitignore ~/.gitignore
 
+echo "Installing Vundle..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
